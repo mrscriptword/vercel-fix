0d70ca013ec8483199da275e7ac40e8721a34f7d
Majalah_Kusam <75724136+mrscriptword@users.noreply.github.com>
2025-12-25 16:19:03 +0700

    Update Kotlin options and versionCode type

--- retail_buah_app/android/app/build.gradle.kts
+++ retail_buah_app/android/app/build.gradle.kts
@@ -15,9 +15,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
+kotlinOptions {
+    jvmTarget = "17"
+}
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
@@ -26,7 +26,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        versionCode = flutter.versionCode.toInt()
         versionName = flutter.versionName
     }
 
