d5613d4a98d311db07e5056678a2456638162e9b
Majalah_Kusam <75724136+mrscriptword@users.noreply.github.com>
2025-12-25 16:48:01 +0700

    Refactor Supabase setup and update CardTheme
    Updated Supabase initialization and fixed CardTheme usage.

--- retail_buah_app/lib/main.dart
+++ retail_buah_app/lib/main.dart
@@ -4,13 +4,12 @@ import 'screens/login.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Initialize Supabase
+
   await Supabase.initialize(
     url: 'https://pfvzenmxslubuhflxkqk.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmdnplbm14c2x1YnVoZmx4a3FrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNjEzMzUsImV4cCI6MjA0OTgzNzMzNX0.x-EE3vT-_IVhPRUn8gHKE6UEz-DXdO2s3mxVzKqD5fQ',
   );
-  
+
   runApp(const MyApp());
 }
 
@@ -38,7 +37,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Retail Buah App',
       debugShowCheckedModeBanner: false,
-      
+
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.light,
@@ -50,8 +49,9 @@ class _MyAppState extends State<MyApp> {
         brightness: Brightness.dark,
         colorSchemeSeed: Colors.cyan,
         scaffoldBackgroundColor: const Color(0xFF121212),
-        // PERBAIKAN DI SINI: Gunakan CardThemeData
-        cardTheme: const CardThemeData(
+
+        // ✅ FIX UTAMA DI SINI
+        cardTheme: const CardTheme(
           color: Color(0xFF1E1E1E),
           elevation: 2,
         ),
